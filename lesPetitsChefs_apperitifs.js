fe00420ba0d58c0062d0bb6c74d0831e7d6b6675
fbremec <fbremec@gmail.com>
2017-09-16 00:05:36 +0200

    les petits chefs project style liste

--- lesPetitsChefs_apperitifs.js
+++ lesPetitsChefs_apperitifs.js
@@ -1,1 +1,6 @@
-var json = {'recettes':[{'nom':'test','difficulte':3,'prepa':'30 min','cuisson':'15 min','nbPersonne':5,'prix':'5 euro'}]};
+var json = {'recettes':[{'nom':'test','difficulte':3,'prepa':'30 min','cuisson':'15 min','nbPersonne':5,'prix':'5 euro'},
+						{'nom':'other test','difficulte':5,'prepa':'30 min','cuisson':'15 min','nbPersonne':2,'prix':'10 euro'},
+						{'nom':'test','difficulte':3,'prepa':'30 min','cuisson':'15 min','nbPersonne':5,'prix':'5 euro'},
+						{'nom':'other test','difficulte':5,'prepa':'30 min','cuisson':'15 min','nbPersonne':2,'prix':'10 euro'},
+						{'nom':'test','difficulte':3,'prepa':'30 min','cuisson':'15 min','nbPersonne':5,'prix':'5 euro'},
+						{'nom':'other test','difficulte':5,'prepa':'30 min','cuisson':'15 min','nbPersonne':2,'prix':'10 euro'}]};

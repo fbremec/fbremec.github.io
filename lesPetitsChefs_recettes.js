6dd9c84b8da4832f2c8c18384678dbffea451df9
fbremec <fbremec@gmail.com>
2017-09-17 15:15:50 +0200

    les petits chefs project : style recette

--- lesPetitsChefs_recettes.js
+++ lesPetitsChefs_recettes.js
@@ -1,10 +1,12 @@
 var jsonRecettesApperitifs = {};
-jsonRecettesApperitifs["other test"] = ({'nom':'other test','difficulte':5,'prepa':'30 min','cuisson':'15 min','nbPersonne':2,'prix':'10 euro'});
+jsonRecettesApperitifs["other test"] = ({"nom":"other test","difficulte":5,"prepa":"30 min","cuisson":"15 min","nbPersonne":2,"prix":"10 euro"});
 
 var jsonRecettesDesserts = {};
-jsonRecettesDesserts["choco cake"] = ({'nom':'choco cake','difficulte':2.5,'prepa':'30 min','cuisson':'20 min','nbPersonne':6,'prix':'5 euro',
-										'ingredients':{
-											'gateau':['200g de margarine','75g de chocolat au lait','75g de chocolat noir','4 oeux','2 grosse cuillère à soupe de farine','100g de sucre','1/2 sachet de levure'],
-											'crème':['40cl de crème liquide entière','150g de chocolat au lait','150g de chocolat noir','1 sachet d\'Agar agar']},
-										'preparation':{
-											'gateau':['Faire fondre la margarine et le chocolat enssemble. Laisser Refroidir','Dans un saladier mélanger les oeux, la farine, le sucre et la levure.','Ajouter à la préparation le chocolat et la margarine fondus, Mélanger.','Verser la pâte dans un moule préalablement beuré.']}});
+jsonRecettesDesserts["Choco cake"] = ({"nom":"Choco cake","difficulte":2.5,"prepa":"30 min","cuisson":"20 min","nbPersonne":6,"prix":"5 euro",
+										"ingredients":{
+											"Gâteau":["200g de margarine","75g de chocolat au lait","75g de chocolat noir","4 oeux","2 grosse cuillère à soupe de farine","100g de sucre","1/2 sachet de levure"],
+											"Crème":["100ml de lait","40cl de crème liquide entière","150g de chocolat au lait","150g de chocolat noir","1 sachet d'Agar agar"]},
+										"preparation":{
+											"Gâteau":["Préchauffer votre four thermostat 6 à 180°C.","Faire fondre la margarine et le chocolat enssemble.", "Laisser Refroidir","Dans un saladier mélanger les oeux, la farine, le sucre et la levure.","Ajouter et mélanger à la préparation le chocolat et la margarine fondus.","Verser la pâte dans un moule préalablement beurré.","Enfourner et laisser cuire à 180°C pendant 20 minutes"],
+											"Crème":["Faire chauffer la crème liquide à feu doux.","lorsque la crème est chaude, ajouter le chocolat.","Remuer jusqu'a ce que le chocolat  soit fondu.","Diluer l'Agar agar dans un peu d'eau froide, puis verser dans la crème et remuer."],
+											"Assemblage":["Une fois le gâteau cuit, verser le lait préalablement chauffé, puis verser la crème.","Laisser refroidir puis reserver au frigo jusqu'a ce que la crème prenne (environ 5h)."]}});
